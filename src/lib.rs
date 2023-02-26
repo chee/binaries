31dc6dfb784b0ed86ca458913c0cf35b057fac0a
chee <yay@chee.party>
2023-02-26 21:20:09 +0000

    simply and make cryptographically secure

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,5 @@
-use rand::distributions::{Distribution, Uniform};
-use rand::Rng;
-use std::iter;
+use rand::{rngs::StdRng, SeedableRng};
+use rand::prelude::SliceRandom;
 
 pub struct Consonant;
 pub struct Vowel;
@@ -14,43 +13,15 @@ const CONSONANT: [char; 21] = [
 
 const VOWEL: [char; 5] = ['a', 'e', 'i', 'o', 'u'];
 
-impl Distribution<char> for Consonant {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> char {
-        let range = Uniform::new(0, 21);
-        CONSONANT[range.sample(rng)]
-    }
-}
-
-impl Distribution<char> for Vowel {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> char {
-        let range = Uniform::new(0, 5);
-        VOWEL[range.sample(rng)]
-    }
-}
-
-impl Distribution<char> for Letter {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> char {
-        Uniform::new(b'A', b'Z').sample(rng).into()
-    }
-}
-
-impl Distribution<usize> for Digit {
-    fn sample<R: Rng + ?Sized>(&self, rng: &mut R) -> usize {
-        Uniform::new(0, 9).sample(rng)
-    }
-}
-
 pub fn word() -> String {
-    let mut crng = rand::thread_rng();
-    let mut vrng = rand::thread_rng();
-    let mut cons = iter::repeat(()).map(|()| crng.sample(Consonant));
-    let mut vows = iter::repeat(()).map(|()| vrng.sample(Vowel));
+    let mut rng = StdRng::from_entropy();
+
     [
-        cons.next().unwrap(),
-        vows.next().unwrap(),
-        cons.next().unwrap(),
-        vows.next().unwrap(),
-        cons.next().unwrap(),
+        *CONSONANT.choose(&mut rng).unwrap(),
+        *VOWEL.choose(&mut rng).unwrap(),
+        *CONSONANT.choose(&mut rng).unwrap(),
+        *VOWEL.choose(&mut rng).unwrap(),
+        *CONSONANT.choose(&mut rng).unwrap(),
     ]
     .iter()
     .collect()
